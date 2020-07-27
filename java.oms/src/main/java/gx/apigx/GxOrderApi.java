59445d907969de39bde9f453e8fc3045e55f5ce7
jasmine1 <qinlili@edanding.com>
2020-07-27 15:03:27 +0800

    test

--- java.oms/src/main/java/gx/apigx/GxOrderApi.java
+++ java.oms/src/main/java/gx/apigx/GxOrderApi.java
@@ -2,8 +2,10 @@ package gx.apigx;
 
 import oms.api.OrderSet;
 import oms.domain.Item;
+import orderutils.ShipOrder;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,12 +17,13 @@ import java.util.List;
  * @Date : Created in 2020/7/27 14:51
  */
 public class GxOrderApi {
+    String orderno = "JOS000123465";
 
     //小B下单，调用OMS内部下单接口
     @Test
     public void orderSet() throws Exception {
         //订单号
-        String orderno = "JOS"+new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+
 
         //组装商品项：下单之前小B需要映射和上架商品、补足库存
         List<Item> items = new ArrayList<>();
@@ -29,6 +32,12 @@ public class GxOrderApi {
 
         //组装并推送订单
         OrderSet.orderSet(orderno,"1021",items);
+    }
+
+    //推送运单回执
+    @Test
+    public void pushShipBack() throws IOException {
+        ShipOrder.shipBackPush("JOS20200727144809");
     }
 
 }
