ea99fb6b0b72385ddbb3fe5792a1a4984b4602de
jasmine1 <qinlili@edanding.com>
2020-08-12 18:19:09 +0800

    test

--- java.ccs/src/main/java/dao/Data.java
+++ java.ccs/src/main/java/dao/Data.java
@@ -6,8 +6,14 @@ package dao;
  * @Date : Created in 2020/8/11 16:01
  */
 public class Data {
-    public static String BASEURL = "http://ccs.backend.daily.yang800.com";
+    // 压测域名（2020-08-12）
 //    public static String BASEURL = "http://ccs.backend.uat.yang800.com";
+//    public static String BASEURL2 = "http://ccs.fen.uat.yang800.com";
+
+    // 日常测试环境域名
+    public static String BASEURL = "http://ccs.backend.daily.yang800.com";
+    public static String BASEURL2 = "http://ccs.fen.daily.yang800.com";
+
     // ccs下单系统域名
     public static String URL = BASEURL+"/xhr/order/submit";
     // ccs推送备案商品
@@ -15,9 +21,7 @@ public class Data {
     // ccs推送账册库存
     public static String URL5 = BASEURL+"/xhr/customsBookItem/upset";
 
-    // 基础域名
-//    public static String BASEURL2 = "http://ccs.fen.uat.yang800.com";
-    public static String BASEURL2 = "http://ccs.fen.daily.yang800.com";
+
     // 回执报文
     public static String URL2 = BASEURL2+"/zjport/callback";
     // 回传核放核注信息
