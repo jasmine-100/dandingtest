d4d453c85d79595b78708f509b1a9e2a443385dd
jasmine1 <qinlili@edanding.com>
2020-11-29 20:26:44 +0800

    test

--- java.erp/src/main/java/api/WmsStockinApi.java
+++ java.erp/src/main/java/api/WmsStockinApi.java
@@ -18,9 +18,10 @@ public class WmsStockinApi {
     @Test
     public void backStockin(){
         List<SkuParam> skuParams = new ArrayList<>();
-        skuParams.add(new SkuParam("G1606550810909","20201128",100,1,1,"2020-11-20 00:00:00","2022-11-10 00:00:00"));
+        skuParams.add(new SkuParam("G1606550810909","20201128",10,1,1,"2020-11-20 00:00:00","2022-11-10 00:00:00"));
+        skuParams.add(new SkuParam("G1606549582326","20201128",20,1,1,"2020-11-20 00:00:00","2022-11-10 00:00:00"));
 
-        StockinOrder stockinOrder = new StockinOrder("ET202011291202040530270431",Data.warehouseCode,Data.ownerCode,0,"1",skuParams);
+        StockinOrder stockinOrder = new StockinOrder("ET202011292014530430667541",Data.warehouseCode,Data.ownerCode,0,"2",skuParams);
 
         ApiClient.doPostJson(Data.urlStockin,null,null,stockinOrder);
     }
--- java.erp/src/main/java/api/WmsStockoutApi.java
+++ java.erp/src/main/java/api/WmsStockoutApi.java
@@ -5,8 +5,11 @@ import dto.stockin.SkuParam;
 import dto.stockout.StockoutOrder;
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @Author： jasmine
@@ -18,8 +21,9 @@ public class WmsStockoutApi {
     @Test
     public void backOrder(){
         List<SkuParam> skuParams = new ArrayList<>();
-        skuParams.add(new SkuParam("G1606625613173","20201128",10,1,1,"2020-11-20 00:00:00","2022-11-10 00:00:00"));
-        StockoutOrder order = new StockoutOrder("OB202011291716330271244295",Data.warehouseCode,Data.ownerCode,"SF","sf123565656",1,skuParams);
+        skuParams.add(new SkuParam("G1606550810909","20201128",10,1,1,"2020-11-20 00:00:00","2022-11-10 00:00:00"));
+        skuParams.add(new SkuParam("G1606549582326","20201128",20,1,2,"2020-11-20 00:00:00","2022-11-10 00:00:00"));
+        StockoutOrder order = new StockoutOrder("OB202011292014530350508707",Data.warehouseCode,Data.ownerCode,"SF","SF"+new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()),1,skuParams);
 
         ApiClient.doPostJson(Data.urlStockout,null,null,order);
     }
