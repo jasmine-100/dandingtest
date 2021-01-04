526f0deefa9d33e0dd4a220c7a1c6d791fbb95f4
jasmine1 <qinlili@edanding.com>
2021-01-04 14:17:46 +0800

    test

--- java.erp/src/main/java/wmsMockDaita/WmsStockinApi.java
+++ java.erp/src/main/java/wmsMockDaita/WmsStockinApi.java
@@ -28,8 +28,8 @@ public class WmsStockinApi {
     @Test
     public void backStockin(){
         List<SkuParam> skuParams = new ArrayList<>();
-        skuParams.add(new SkuParam("SKU201225005","202012270001",10,1,1,"2020-12-01","2020-12-31"));
-        skuParams.add(new SkuParam("SKU201225005","202012270002",10,2,2,"2020-12-01","2020-12-31"));
+        skuParams.add(new SkuParam("SKU201229109","202012270001",90,1,1,"2020-12-01","2020-12-31"));
+        skuParams.add(new SkuParam("SKU201229109","202012270001",10,2,2,"2020-12-01","2020-12-31"));
 
         StockinOrder stockinOrder = new StockinOrder(orderno,Data.warehouseCode,Data.ownerCode,1,"1",skuParams,"2020-10-10 12:00:00","2020-10-20 15:00:00");
 
