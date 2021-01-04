8bf053ef8435f1ba696420d5fcda2dba1ec8bc6a
jasmine1 <qinlili@edanding.com>
2021-01-04 18:07:20 +0800

    test

--- java.erp/src/main/java/wmsMockDaita/WmsStockinApi.java
+++ java.erp/src/main/java/wmsMockDaita/WmsStockinApi.java
@@ -18,7 +18,7 @@ import java.util.List;
  * @Date : Created in 2020/11/28 17:40
  */
 public class WmsStockinApi {
-    String orderno = "ET202101040929370009899533";
+    String orderno = "ET202101041724570474493812";
 
     @Test
     public void updateStatusIn(){
@@ -28,10 +28,10 @@ public class WmsStockinApi {
     @Test
     public void backStockin(){
         List<SkuParam> skuParams = new ArrayList<>();
-        skuParams.add(new SkuParam("SKU201229109","202012270001",90,1,1,"2020-12-01","2020-12-31"));
-        skuParams.add(new SkuParam("SKU201229109","202012270001",10,2,2,"2020-12-01","2020-12-31"));
+        skuParams.add(new SkuParam("SKU201225005","210104170202",900,1,1,"2020-12-01","2020-12-31"));
+//        skuParams.add(new SkuParam("SKU201229108","202012270001",20,2,2,"2020-12-01","2020-12-31"));
 
-        StockinOrder stockinOrder = new StockinOrder(orderno,Data.warehouseCode,Data.ownerCode,1,"1",skuParams,"2020-10-10 12:00:00","2020-10-20 15:00:00");
+        StockinOrder stockinOrder = new StockinOrder(orderno,Data.warehouseCode,Data.ownerCode,0,"1",skuParams,"2020-10-10 12:00:00","2020-10-20 15:00:00");
 
         ApiClient.doPostJson(Data.urlStockin,null,null,stockinOrder);
     }
