7ed0ecffee964292dd7ea4cd754dd81c33155fc3
jasmine1 <qinlili@edanding.com>
2021-02-19 18:39:38 +0800

    test

--- java.erp/src/test/java/qimen/GoodApi.java
+++ java.erp/src/test/java/qimen/GoodApi.java
@@ -12,10 +12,10 @@ public class GoodApi extends Data {
 
     @Test
     public void goodAdd(){
-        String str = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-        String goodNo = "SKM" + str;
-//        String goodNo = "SKM20210218155423";
-        Good good = new Good(goodNo,"鼠标",str,warehouseCode,ownerCode);
+//        String str = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+//        String goodNo = "SKM" + str;
+        String goodNo = "SKM20210219183551";
+        Good good = new Good(goodNo,"鼠标",goodNo,warehouseCode,ownerCode);
         Params params = new Params(goodNo,"3",good);
 
         ApiClient.doPostJson(URL,null,null,params);
