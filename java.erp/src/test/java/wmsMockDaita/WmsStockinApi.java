fdca965cb70e9fcc1652adf7a21ea826263f2b32
jasmine1 <qinlili@edanding.com>
2021-02-19 15:32:41 +0800

    test

--- java.erp/src/test/java/wmsMockDaita/WmsStockinApi.java
+++ java.erp/src/test/java/wmsMockDaita/WmsStockinApi.java
@@ -26,15 +26,15 @@ public class WmsStockinApi extends Data {
     public void backStockin(){
         List<SkuParam> skuParams = new ArrayList<>();
         skuParams.add(new SkuParam("SKU2101141455","2021021801001",2000,1,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141455","2021021801002",1000,1,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141455","2021021801003",3000,1,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141455","2021021801004",2000,2,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141455","2021021801005",2000,2,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141456","2021021802001",1000,1,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141456","2021021802002",1000,1,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141456","2021021802003",1000,1,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141456","2021021802004",1000,2,1,"2020-10-10","2022-10-10"));
-//        skuParams.add(new SkuParam("SKU2101141456","2021021802005",1000,2,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141455","2021021801002",1000,1,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141455","2021021801003",3000,1,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141455","2021021801004",2000,2,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141455","2021021801005",2000,2,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141456","2021021802001",1000,1,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141456","2021021802002",1000,1,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141456","2021021802003",1000,1,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141456","2021021802004",1000,2,1,"2020-10-10","2022-10-10"));
+        skuParams.add(new SkuParam("SKU2101141456","2021021802005",1000,2,1,"2020-10-10","2022-10-10"));
 
         StockinOrder stockinOrder = new StockinOrder(orderno,warehouseCode,ownerCode,1,"1",skuParams,"2020-10-10 12:00:00","2021-01-27 10:00:00");
 
