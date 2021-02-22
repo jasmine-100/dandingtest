1e493856f09202308733ad559b10ec07686f4de1
jasmine1 <qinlili@edanding.com>
2021-02-22 10:34:38 +0800

    test

--- java.erp/src/test/java/qimen/Data.java
+++ java.erp/src/test/java/qimen/Data.java
@@ -9,13 +9,12 @@ public class Data {
     //完税-开启效期
 //    String warehouseCode = "JHS8029fehahaWmsWanshui";//wms大贸仓
 //    String ownerCode = "hahaWmsWanshui";//货主编码
-    //完税-关闭效期
-//    String warehouseCode = "JHS8029fehahaWmsWanshui";//wms大贸仓
-    String warehouseCode = "JHS26c6a1jinyiQimen";//qimen仓
-    String ownerCode = "jinyiQimen";//货主编码
-    //保税
-//    String warehouseCode = "BJCQ24426aHainanHuozhu1";//海南保税仓1
-//    String ownerCode = "HainanHuozhu1";//货主编码
+    //金义奇门仓
+//    String warehouseCode = "JHS26c6a1jinyiQimen";//qimen仓
+//    String ownerCode = "jinyiQimen";//货主编码
+    //海南奇门仓
+    String warehouseCode = "BJCQb19b4chainanHuozhu0222";//海南保税仓1
+    String ownerCode = "hainanHuozhu0222";//货主编码
 
     //
 //    String warehouseCode = "JHS26c6a1jinyiQimen";//qimen仓
