f027745ad66abff0d52e4d9c8b70fd53cfe8b830
jasmine1 <qinlili@edanding.com>
2021-02-22 18:12:03 +0800

    test

--- java.erp/src/test/java/erp/qimen/Data.java
+++ java.erp/src/test/java/erp/qimen/Data.java
@@ -13,12 +13,12 @@ public class Data {
 //    String warehouseCode = "JHS26c6a1jinyiQimen";//qimen仓
 //    String ownerCode = "jinyiQimen";//货主编码
     //海南奇门仓
-//    String warehouseCode = "BJCQb19b4chainanHuozhu0222";//海南保税仓1
-//    String ownerCode = "hainanHuozhu0222";//货主编码
+    String warehouseCode = "BJCQb19b4chainanHuozhu0222";//海南保税仓1
+    String ownerCode = "hainanHuozhu0222";//货主编码
 
     //熊本熊公司
-    String warehouseCode = "JHS95b4cdjinyiQimenHuozhu0222";//金义奇门仓
-    String ownerCode = "jinyiQimenHuozhu0222";//熊熊的金义奇门货主
+//    String warehouseCode = "JHS95b4cdjinyiQimenHuozhu0222";//金义奇门仓
+//    String ownerCode = "jinyiQimenHuozhu0222";//熊熊的金义奇门货主
 //    String warehouseCode = "BJCQ487c4chainanQimenHuozhu0222";//海南奇门仓
 //    String ownerCode = "hainanQimenHuozhu0222";//熊熊的海南奇门货主
 
--- java.erp/src/test/java/erp/qimen/GoodApi.java
+++ java.erp/src/test/java/erp/qimen/GoodApi.java
@@ -4,15 +4,17 @@ import org.testng.annotations.Test;
 import utils.client.ApiClient;
 import erp.qimen.dto.Params;
 import erp.qimen.dto.good.Good;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class GoodApi extends Data {
 
     @Test
     public void goodAdd(){
-//        String str = new SimpleDateFormat("yyMMddHHmmss").format(new Date());
-//        String goodNo = "SK" + str;
-        String goodNo = "SK210222112519";
-        Good good = new Good(goodNo,"小白兔",goodNo+"111",warehouseCode,ownerCode);
+        String str = new SimpleDateFormat("yyMMddHHmmss").format(new Date());
+        String goodNo = "SK" + str;
+//        String goodNo = "SK210222112519";
+        Good good = new Good(goodNo,"小白兔",str,warehouseCode,ownerCode);
         Params params = new Params(goodNo,"3",good);
 
         ApiClient.doPostJson(URL,null,null,params);
