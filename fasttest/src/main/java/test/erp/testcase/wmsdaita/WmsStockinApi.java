987e6155c79d9d02fbf25f6b89f2bb3ccebc7916
jasmine1 <qinlili@edanding.com>
2021-03-10 15:03:40 +0800

    test

--- fasttest/src/main/java/test/erp/testcase/wmsdaita/WmsStockinApi.java
+++ fasttest/src/main/java/test/erp/testcase/wmsdaita/WmsStockinApi.java
@@ -1,11 +1,14 @@
 package test.erp.testcase.wmsdaita;
 
+import jxl.Sheet;
+import jxl.Workbook;
 import org.testng.annotations.Test;
 import test.erp.testcase.wmsdaita.dto.stockin.SkuParam;
 import test.erp.testcase.wmsdaita.dto.stockin.StockinOrder;
 import utils.client.apiclientDTO.ApiClient;
 import utils.database.ErpOrder;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,4 +36,31 @@ public class WmsStockinApi extends Data {
         ApiClient.doPostJson(urlStockin,null,null,stockinOrder);
     }
 
+    // 读取excel商品参数
+    @Test
+    public void backStockin2() {
+        List<SkuParam> skuParams = new ArrayList<>();
+        try {
+            String excelFileName = "C:/Users/jasmine/Desktop/比瑞吉入库单回传参数.xls";
+            Workbook wb = Workbook.getWorkbook(new File(excelFileName));
+            Sheet sheet = wb.getSheet(0);
+            for (int i =1;i<sheet.getRows();i++){
+                String sku = sheet.getCell(0,i).getContents();
+                int num = Integer.valueOf(sheet.getCell(1,i).getContents());
+                String productDate = sheet.getCell(2,i).getContents();
+                String expireDate = sheet.getCell(3,i).getContents();
+                int inventoryType = Integer.valueOf(sheet.getCell(4,i).getContents());
+                String bathcode = sheet.getCell(5,i).getContents();
+                SkuParam skuParam = new SkuParam(sku,bathcode,num,inventoryType,i,productDate,expireDate,false);
+                skuParams.add(skuParam);
+                System.out.println(skuParam);
+            }
+            StockinOrder stockinOrder = new StockinOrder("ET202103101012330805255279","ASN21031000000301","DT_MNWMS0308","JJCS",0,"1",skuParams,"2020-10-10 12:00:00","2021-01-27 10:00:00");
+
+            ApiClient.doPostJson(urlStockin,null,null,stockinOrder);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
