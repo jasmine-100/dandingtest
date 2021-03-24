2154b33d515d9491fe527bed70f00aacd402b6a5
jasmine1 <qinlili@edanding.com>
2021-03-24 10:00:19 +0800

    test

--- fasttest/src/main/java/utils/client/gatewayDTO/BaseTest.java
+++ fasttest/src/main/java/utils/client/gatewayDTO/BaseTest.java
@@ -16,7 +16,7 @@ public abstract class BaseTest {
     //开发环境
     protected GatewayHttpClient devClient = new GatewayHttpClient("http://192.168.20.200:9195","96A63530DA0C49BB9FABB66ED40FB3C7","F6A99B36E4D24817AB037237454893D9",true);
     //测试环境
-    protected GatewayHttpClient testClient = new GatewayHttpClient("http://danding-gateway-pre.yang800.com","96A63530DA0C49BB9FABB66ED40FB3C7","F6A99B36E4D24817AB037237454893D9",true);
+    protected GatewayHttpClient testClient = new GatewayHttpClient("http://danding-gateway-test.yang800.com.cn","96A63530DA0C49BB9FABB66ED40FB3C7","F6A99B36E4D24817AB037237454893D9",true);
     //线上环境
     protected GatewayHttpClient onlineClient = new GatewayHttpClient("http://danding-gateway.yang800.com","9E514E70AD7D485986D687F64616C662","33F14542BB274284B63147E6C8F3DF9E",true);
     protected GatewayHttpClient client;
