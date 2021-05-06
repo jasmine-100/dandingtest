585319f1991b2e5d32cf26fe6a6ebe25471e7de3
jasmine1 <qinlili@edanding.com>
2021-05-06 11:53:21 +0800

    test

--- fasttest/src/main/java/test/erp/testcase/outQimen/dto/stockin/InOrderDetailParam.java
+++ fasttest/src/main/java/test/erp/testcase/outQimen/dto/stockin/InOrderDetailParam.java
@@ -9,12 +9,14 @@ public class InOrderDetailParam {
     String billCurrency;
     double unitPrice;
     int planQuantity;
+    int inventoryType;//1 正品，2 次品
 
-    public InOrderDetailParam(String sku, double unitPrice, int planQuantity) {
+    public InOrderDetailParam(String sku, double unitPrice,int inventoryType, int planQuantity) {
         this.sku = sku;
         this.goodsName = "";
         this.billCurrency = "RMB";
         this.unitPrice = unitPrice;
+        this.inventoryType = inventoryType;
         this.planQuantity = planQuantity;
     }
 }
