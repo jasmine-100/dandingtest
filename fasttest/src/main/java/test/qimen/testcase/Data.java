0c8d66dbec6a961bbcaf072215e9deafef19d005
jasmine1 <qinlili@edanding.com>
2021-05-07 17:52:21 +0800

    test

--- fasttest/src/main/java/test/qimen/testcase/Data.java
+++ fasttest/src/main/java/test/qimen/testcase/Data.java
@@ -16,9 +16,9 @@ public class Data {
 //    public static String ownerCode = "hainanHuozhu0222";//海南奇门货主
 //    String logicWarehouseCode = "JHS597d78HahaJinyiQimen1";//金义奇门仓（强效期）
 //    String ownerCode = "HahaJinyiQimen1";//哈哈金义奇门货主（强效期）
-//    String logicWarehouseCode = "JHS71d3a7TuoguanQ";//金义奇门仓（强效期）
-//    String ownerCode = "TuoguanQ";//哈哈金义奇门货主（强效期）
+    String logicWarehouseCode = "JHS71d3a7TuoguanQ";//金义奇门仓（强效期）
+    String ownerCode = "TuoguanQ";//哈哈金义奇门货主（强效期）
 
-    String logicWarehouseCode = "STSf4ae0dyaceqimen";//金义奇门仓（强效期）
-    String ownerCode = "yaceqimen";//哈哈金义奇门货主（强效期）
+//    String logicWarehouseCode = "STSf4ae0dyaceqimen";//金义奇门仓（强效期）
+//    String ownerCode = "yaceqimen";//哈哈金义奇门货主（强效期）
 }
