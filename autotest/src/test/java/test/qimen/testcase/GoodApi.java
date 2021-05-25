2deaba7cfa3247fb00390c3ffe0b1f2012852066
jasminesmile <897247986@qq.com>
2021-05-25 20:36:13 +0800

    test

---
+++ autotest/src/test/java/test/qimen/testcase/GoodApi.java
@@ -0,0 +1,29 @@
+package test.qimen.testcase;
+
+import org.testng.annotations.Test;
+import test.qimen.testcase.dto.Param;
+import test.qimen.testcase.dto.good.Good;
+import test.qimen.testcase.dto.good.Item;
+import utils.client.apiclientDTO.ApiClient;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author： jasmine
+ * @Description :同步货品的接口
+ * @Date : Created in 2021/2/24 17:54
+ */
+public class GoodApi extends Data{
+
+    @Test
+    public void goodCreate(){
+        Param param = new Param("singleitem.synchronize");
+
+        String sku = "QSKU"+new SimpleDateFormat("yyMMddHHmmss").format(new Date());
+        Good good = new Good("add",logicWarehouseCode,ownerCode,new Item(sku,"苹果手机","PP"+ sku,"SO"+sku));
+
+        ApiClient.doPostXml(URL,param,null,good);
+    }
+
+}
---
+++ autotest/src/test/java/test/wmsDaita/testcase/RongqiApi.java
@@ -0,0 +1,32 @@
+package test.wmsDaita.testcase;
+
+import org.testng.annotations.Test;
+import test.wmsDaita.testcase.dto.rq.RongQi;
+import utils.client.gatewayDTO.GatewayHttpClient;
+import utils.client.gatewayDTO.BaseTest;
+
+/**
+ * @Author： jasmine
+ * @Description :
+ * @Date : Created in 2021/2/23 14:25
+ */
+public class RongqiApi extends LoginWms {
+    int startno = 11;
+    int endno =20;
+    int temp = startno;
+
+    @Test
+    public void addRongqis(){
+        for (int i=startno;i<=endno;i++){
+            temp = i;
+            addRongqi();
+        }
+    }
+
+    @Test
+    public void addRongqi(){
+        RongQi rongQi = new RongQi("rq"+temp,"容器"+temp);
+        GatewayHttpClient.send(client,"/dt-wms-portal/container/add",accessToken,rongQi);
+    }
+
+}
